1c2f8a9c61b7ab21d024d56259901e3057f9f3b3
Dharun V <vdharun11@gmail.com>
2025-06-26 16:11:19 +0530

    Update ExpenseContainer.jsx

--- React/Frontend/src/Component/ExpenseContainer.jsx
+++ React/Frontend/src/Component/ExpenseContainer.jsx
@@ -55,7 +55,7 @@ if(!response.ok){
 }
 async function deleteExpensedb(id){
     try{
-        const response = await fetch(`https://expense-tracker-backend-64c7.onrender.com/expense${id}`,{
+        const response = await fetch(`https://expense-tracker-backend-64c7.onrender.com/expense`,{
             method:'DELETE',
         });
 
